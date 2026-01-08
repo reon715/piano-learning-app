4981a346b79f2e4498344fcaf545ed5ab27efc1a
reon715 <Reon.Takamura@iswa.wa.edu.au>
2026-01-08 15:23:05 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ const button = document.getElementById("change-btn");
 button.addEventListener("click", () => {
   message.textContent = "ボタンがクリックされました！";
 });
+const keys = document.querySelectorAll(".key");
+
+keys.forEach((key) => {
+  key.addEventListener("click", () => {
+    const note = key.dataset.note;
+    console.log("押された音:", note);
+  });
+});
+
